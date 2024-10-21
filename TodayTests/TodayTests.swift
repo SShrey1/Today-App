eb9067f73aa17e90638a0e308aef502f6a6df977
SShrey1 <palishrey@gmail.com>
2024-10-21 23:58:39 +0530

    Initial Commit

---
+++ TodayTests/TodayTests.swift
@@ -0,0 +1,17 @@
+//
+//  TodayTests.swift
+//  TodayTests
+//
+//  Created by user@59 on 21/10/24.
+//
+
+import Testing
+@testable import Today
+
+struct TodayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
